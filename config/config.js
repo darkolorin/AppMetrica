999d23e07a29a9e4d6c34e2d34b0232d64482b3b
Aleksei Moiseenkov <darkolorin@Alekseis-MacBook-Pro.local>
2015-12-05 03:43:22 +0300

    working oauth

---
+++ config/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  app_key: 'ac05be5c7d2b48719b52e0ff1b44417f',
+  app_secret: '4cb9d6c47f104d46a7b47d4788d125a1',
+  redirect_uri: 'https://oauth.yandex.ru/verification_code',
+  code_uri: 'https://oauth.yandex.ru/authorize',
+  auth_uri: 'https://oauth.yandex.ru/token',
+  state: 'AppMetricaStateString'
+};
--- index.ios.js
+++ index.ios.js
@@ -7,6 +7,8 @@
 var React = require('react-native');
 var url = require('url');
 var config = require('./config/config.js');
+var DeviceInfo = require('react-native-device-info');
+var formurlencoded = require('form-urlencoded');
 
 var {
   AppRegistry,
@@ -17,10 +19,11 @@ var {
 } = React;
 
 var OAUTH_URL = [
-  config.code_uri,
-  '?client_id=' + config.app_key,
-  '&response_type=code',
-  '&redirect_uri=' + config.redirect_uri
+   config.code_uri,
+  '?response_type=code&client_id=' + config.app_key,
+  '&state=' + config.state,
+  '&device_id='+ DeviceInfo.getUniqueID(),
+  '&device_name=' + DeviceInfo.getDeviceId()
 ].join('');
 
 var AppMetrica = React.createClass({
@@ -37,25 +40,76 @@ var AppMetrica = React.createClass({
 
     render: function() {
 
-      if (this.state.login) {
-          return (
-            <HomeView />
-          );
-      }
+    if (this.state.login) {
+        return (
+          <View style = {styles.container}>
+            <Text style = {styles.text}> {this.state.token} </Text>
+          </View>
+        );
+    }
 
-      return (
-        <View style={{flex: 1}}>
-          <WebView
-            ref={'webview'}
-            url={OAUTH_URL}
-            automaticallyAdjustContentInsets={false}
-            onNavigationStateChange={this.onNavigationStateChange.bind(this)}
-            startInLoadingState={true}
-            scalesPageToFit={true} />
-        </View>
-      );
-    },
+    return (
+      <View style={{flex: 1}}>
+        <WebView
+          ref={'webview'}
+          url={OAUTH_URL}
+          automaticallyAdjustContentInsets={false}
+          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+          startInLoadingState={true}
+          scalesPageToFit={true} />
+      </View>
+    );
+  },
+
+  onNavigationStateChange: function (navState) {
+    var urlObj = url.parse(navState.url, true);
+    top:
+    if (urlObj.pathname == url.parse(config.redirect_uri).pathname) {
+      var code = urlObj.query.code;
+      console.log(code);
+      var postObj = formurlencoded.encode({
+        grant_type: 'authorization_code',
+        code: code,
+        client_id: config.app_key,
+        client_secret: config.app_secret,
+        device_id: DeviceInfo.getUniqueID(),
+        device_name: DeviceInfo.getDeviceId()
+      });
+
+
+      fetch('https://oauth.yandex.ru/token', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: postObj
+      }).then((response) => response.json())
+        .then((responseData) => {
+          console.log(responseData);
+          this.setState({
+            login: true,
+            token: responseData.access_token
+          })
+          })
+        .catch((err) => console.log(err))
+        .done();
+      break top;
+    }
+  }
 
 });
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F6F6EF',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#00AA00',
+  }
+});
+
 
 AppRegistry.registerComponent('AppMetrica', () => AppMetrica);
